093887002de363f62511253a897086f9cb6b87a2
jihun <jihun332@gmai.com>
2026-07-02 11:07:32 +0900

    feat: CoreNetwork 기본 인터페이스 타입 추가 - #8

---
+++ NyangJup/Packages/Package/Projects/Core/Network/Interface/Sources/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  NJPackage
+//
+//  Created by 정지훈 on 7/1/26.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
