502b3f030748b0a5bf05496a760a66fd05cc448d
jihun <jihun332@gmai.com>
2026-07-06 16:18:18 +0900

    build: HomeExample 앱 실행 구성 연결 - #6

--- NyangJup/App/Examples/HomeExample/HomeExampleApp.swift
+++ NyangJup/App/Examples/HomeExample/HomeExampleApp.swift
@@ -1,11 +1,22 @@
 import SwiftUI
+
 import FeatureHome
+import FeatureHomeInterface
+import DomainProfileTesting
+import DomainCatsTesting
 
 @main
 struct HomeExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView(viewModel: HomeViewModel())
+            NavigationStack {
+                HomeView(
+                    viewModel: HomeViewModel(
+                        catsClient: .test,
+                        profileClient: .test
+                    )
+                )
+            }
         }
     }
 }
